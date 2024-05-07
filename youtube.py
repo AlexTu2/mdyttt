2f1bf78c74f48e8e3829a8e9a56596c5fb2200b0
Alex Tu <6798052+AlexTu2@users.noreply.github.com>
2024-05-07 01:53:20 -0400

    feat: Save credentials

--- youtube.py
+++ youtube.py
@@ -4,12 +4,29 @@
 # https://developers.google.com/explorer-help/code-samples#python
 
 import os
+import pickle  # Import the pickle module to serialize credentials
 
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+def get_credentials():
+    # Check if credentials file exists
+    if os.path.exists("youtube_credentials.pickle"):
+        # Load credentials from file
+        with open("youtube_credentials.pickle", "rb") as token:
+            credentials = pickle.load(token)
+    else:
+        # If credentials file does not exist, run the authentication flow
+        flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+            "YOUR_CLIENT_SECRET_FILE.json", scopes)
+        credentials = flow.run_local_server(port=0)
+        # Save credentials to file
+        with open("youtube_credentials.pickle", "wb") as token:
+            pickle.dump(credentials, token)
+    return credentials
 
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -20,10 +37,10 @@ def main():
     api_version = "v3"
     client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_local_server(port=0)
+    # Get credentials
+    credentials = get_credentials()
+
+    # Create an API client using the stored credentials
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials)
 
